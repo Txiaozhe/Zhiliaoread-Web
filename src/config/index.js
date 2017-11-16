9d2484aefba22343d50c92eb78088d9f9c05474d
TangXiaoji <txiaozhe@gmail.com>
2017-11-17 00:44:55 +0800

    统一url配置

--- src/config/index.js
+++ src/config/index.js
@@ -6,5 +6,7 @@
 'use strict';
 
 import string from './string';
+import url from './url';
 
 export const String = string;
+export const Url = url;
---
+++ src/config/url.js
@@ -0,0 +1,13 @@
+/**
+ * Creator: Tang Xiaoji
+ * Time: 2017-11-15
+ */
+
+'use strict';
+
+const url = {
+    host: '',
+    home: ''
+}
+
+export default url;
