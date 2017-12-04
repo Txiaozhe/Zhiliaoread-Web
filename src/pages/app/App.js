c1765d07d586b9b590f1fa369c4e1b9bd5227f73
TangXiaoji <txiaozhe@gmail.com>
2017-12-04 09:45:28 +0800

    修复引用错误、删除无用代码

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -8,31 +8,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import Test from '../test/Test';
-
 import Home from '../home/Home';
 
-/*class App extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
     return (
-      <Test />
+      <Home />
     );
   }
-}*/
-
-class App extends React.Component {
-  
-
-
-render() {
-  return (
-    <Home />
-  );
-}
 }
 
 export default connect()(App);
